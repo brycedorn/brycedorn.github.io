39c657165d7f4895274cd4355049b85e0398f954
Bryce Dorn <brycedorn@gmail.com>
2013-03-26 03:29:48 -0500

    added anims updated about

--- js/imageshow.js
+++ js/imageshow.js
@@ -1,6 +1,18 @@
 function showImage(){
 	document.getElementById('loadingImage').style.visibility='visible';
+	document.getElementById('loadingImage').style.bottom='0px';
 }
 function hideImage(){
 	document.getElementById('loadingImage').style.visibility='hidden';
+	document.getElementById('loadingImage').style.bottom='700px';
+}
+
+
+function timeredirect()
+{
+    var t = setTimeout("redirect_to('')", 1500);
+}
+function redirect_to(url)
+{
+     document.location = url;
 }
