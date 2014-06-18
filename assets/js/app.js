6d107fd881577cd72693c15e6e1c85423c6b5d9a
Bryce Dorn <brycedorn@gmail.com>
2014-06-17 23:55:53 -0500

    messing with book theme

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-$("#head").fitText(0.7);
+$(document).ready(function () {
+  rotate($("#head"),90);
+});
+
+
+// TY stackoverflow
+function rotate($el, degrees) {
+  $el.css({
+    '-webkit-transform': 'rotate(' + degrees + 'deg)',
+    '-moz-transform': 'rotate(' + degrees + 'deg)',
+    '-ms-transform': 'rotate(' + degrees + 'deg)',
+    '-o-transform': 'rotate(' + degrees + 'deg)',
+    'transform': 'rotate(' + degrees + 'deg)',
+    'zoom': 1
+  });
+}
