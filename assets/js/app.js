3e398d5c9b4f4f93b8bd45397924650cadacc25b
Bryce Dorn <brycedorn@gmail.com>
2014-07-03 23:35:01 -0500

    minor updates, gets rid of works that shouldnt be on there

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
   var h = $(window).height();
   $(".closed").children().hide();
   $("body").fadeIn(frames * 2);
-  $("#head").fitText(0.255);
-  $("#blog").fitText(4.2);
   $("#left").height(h);
   $("#blog").height(h - 100);
   $("#works").height(h - 100);
@@ -21,17 +19,18 @@ $(document).ready(function() {
     } else openPage($("#contact"));
   });
 
+  var myScroll = new IScroll('#blog');
   $("#blog").find(".content").delay(600).fadeIn(frames * 2);
 
   // get_posts();
 
   function openPage(t) {
     if (!$(t).hasClass("open")) {
-      $(".open").parent().switchClass("pure-u-17-24", "pure-u-1-24", frames, anim)
+      $(".open").parent().switchClass("pure-u-20-24", "pure-u-1-24", frames, anim)
       $(".open").children().hide();
       $(".open").switchClass("open", "closed", frames, anim);
       $(t).switchClass("closed", "open", frames, anim);
-      $(t).parent().switchClass("pure-u-1-24", "pure-u-17-24", frames, anim);
+      $(t).parent().switchClass("pure-u-1-24", "pure-u-20-24", frames, anim);
       $(t).children().delay(frames / 2).show("fade", "slow");
     }
   }
