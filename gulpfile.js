2eeec1d8efc8c3012c4784611488afc06d797fff
Bryce Dorn <brycedorn@gmail.com>
2015-11-27 18:28:18 -0500

    adds gulp/npm

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+
+gulp.task('js', function() {
+  gulp.src('src/js/main.js')
+    .pipe(browserify({transform:'reactify'}))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('css', function() {
+  gulp.src(['src/css/styles.css','src/css/font-awesome.min.css'])
+  	.pipe(concat('styles.css'))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('fonts', function() {
+  gulp.src('src/fonts/fontawesome-webfont.woff2')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('copy', function() {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('default',['js', 'css', 'fonts', 'copy']);
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.*', ['default']);
+});
