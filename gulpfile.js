c3f51dfea57d9470062c473c2377a3388263fb14
Bryce Dorn <brycedorn@gmail.com>
2015-12-02 16:04:05 -0600

    adds zepto to packages, adds uglify to js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('js', function() {
   gulp.src('src/js/main.js')
     .pipe(browserify({transform:'reactify'}))
+    .pipe(uglify())
     .pipe(concat('main.js'))
     .pipe(gulp.dest('dist/js'));
 });
