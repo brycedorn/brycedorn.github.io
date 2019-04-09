22cf7d928494d8e4078daf6e2a15993bec51cbb9
Bryce Dorn <brycedorn@gmail.com>
2019-04-08 23:33:59 -0500

    add spotify

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import posed, { PoseGroup } from "react-pose";
 
 const ga = {
   initialize: () => {},
@@ -14,18 +15,57 @@ import gitlab from "../img/gitlab.svg";
 import twitter from "../img/twitter.svg";
 import lastfm from "../img/lastfm.svg";
 import devto from "../img/dev-dot-to.svg";
+import spotify from "../img/spotify.svg";
 import bg from "../img/bg.png";
 
 import "../styles/index.scss";
 
 const links = [
-  { url: "https://www.flickr.com/photos/_burce", icon: flickr },
-  { url: "http://www.last.fm/user/BDORN", icon: lastfm },
-  { url: "https://twitter.com/combatfetus", icon: twitter },
-  { url: "https://dev.to/bryce", icon: devto },
-  { url: "https://gitlab.com/brycedorn", icon: gitlab },
-  { url: "https://github.com/brycedorn", icon: github }
+  {
+    url: "https://twitter.com/combatfetus",
+    icon: twitter
+  },
+  {
+    url: "https://dev.to/bryce",
+    icon: devto
+  },
+  {
+    url: "https://gitlab.com/brycedorn",
+    icon: gitlab
+  },
+  {
+    url: "https://github.com/brycedorn",
+    icon: github
+  },
+  {
+    url: "http://www.last.fm/user/BDORN",
+    icon: lastfm
+  },
+  {
+    url: "https://open.spotify.com/user/combatfetus?si=1UVE0T7JR2SsHwfB4W_x8w",
+    icon: spotify
+  },
+  {
+    url: "https://www.flickr.com/photos/_burce",
+    icon: flickr
+  }
 ];
+
+const Link = posed.a({
+  enter: {
+    x: 0,
+    scale: 1,
+    transition: {
+      duration: 100,
+      ease: "easeInOut"
+    }
+  },
+  exit: {
+    x: -60
+  },
+  hoverable: true,
+  hover: { scale: 1.15 }
+});
 
 class Index extends Component {
   componentWillMount() {
@@ -41,23 +81,25 @@ class Index extends Component {
 
     return (
       <div id="react-root">
+        <div className="links">
+          <PoseGroup>
+            {links.map((item, index) => (
+              <Link
+                className="link"
+                dangerouslySetInnerHTML={{ __html: item.icon }}
+                href={item.url}
+                key={index}
+                index={index}
+                target="_blank"
+              />
+            ))}
+          </PoseGroup>
+        </div>
+        <Legos />
         <div
           className="background-scaffold"
           style={{ backgroundImage: `url(${bg})` }}
         />
-        <Legos />
-        <div className="links">
-          {links.map((item, index) => (
-            <a
-              className="link"
-              dangerouslySetInnerHTML={{ __html: item.icon }}
-              href={item.url}
-              key={index}
-              index={index}
-              target="_blank"
-            />
-          ))}
-        </div>
       </div>
     );
   }
