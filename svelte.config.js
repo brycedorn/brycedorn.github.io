7374e1579e662bb25bc60a3d2e231b3e0e30ad48
Bryce Dorn <bryce.dorn@uber.com>
2025-01-15 14:16:04 +0100

    inline

--- svelte.config.js
+++ svelte.config.js
@@ -6,11 +6,13 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
 			fallback: '200.html' // may differ from host to host
-		})
+		}),
+		output: {
+			bundleStrategy: 'inline'
+		}
 	}
 };
 
